41e6aa2843845bb55590c14a14ad4af2915989f6
Jim Ellenberger <jim.ellenberger@gmail.com>
2020-01-11 15:23:49 -0500

    Format all python files with black

--- djangox_project/urls.py
+++ djangox_project/urls.py
@@ -3,14 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('django.contrib.auth.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('', include('pages.urls')),
+    path("admin/", admin.site.urls),
+    path("users/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("", include("pages.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-] + urlpatterns
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
